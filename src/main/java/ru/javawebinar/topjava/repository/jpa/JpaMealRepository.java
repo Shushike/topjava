ecb074e82fcffc9821a5d0d03bb63b97ad451287
Shushike <jr.shushike@gmail.com>
2020-10-25 22:36:34 +0500

    HW04: Meal for Hibernate, JpaMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -1,37 +1,71 @@
 package ru.javawebinar.topjava.repository.jpa;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.Util;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TemporalType;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaMealRepository implements MealRepository {
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
+        if (meal.isNew()) {
+            User ref = em.getReference(User.class, userId);
+            meal.setUser(ref);
+            em.persist(meal);
+            return meal;
+        } else {
+            if (meal.getUser()!=null && meal.getUser().getId()==userId)
+                return em.merge(meal);
+        }
         return null;
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
-        return false;
+        return em.createNamedQuery(Meal.DELETE)
+                .setParameter("id", id)
+                .setParameter(Meal.userIdName, userId)
+                .executeUpdate() != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        Map<String,Object> props = new HashMap<>();
+        props.put(Meal.userIdName, userId);
+        return em.find(Meal.class, id, props);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return em.createNamedQuery(Meal.ALL_SORTED, Meal.class)
+                .setParameter(Meal.userIdName, userId)
+                .getResultList();
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return em.createNamedQuery(Meal.BY_TIME, Meal.class)
+                .setParameter(Meal.userIdName, userId)
+                .setParameter(Meal.startTimeName, Util.convertToDate(startDateTime), TemporalType.TIMESTAMP)
+                .setParameter(Meal.endTimeName, Util.convertToDate(endDateTime), TemporalType.TIMESTAMP)
+                .getResultList();
     }
 }
--- src/main/java/ru/javawebinar/topjava/util/Util.java
+++ src/main/java/ru/javawebinar/topjava/util/Util.java
@@ -2,11 +2,36 @@ package ru.javawebinar.topjava.util;
 
 import org.springframework.lang.Nullable;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class Util {
     private Util() {
     }
 
     public static <T extends Comparable<T>> boolean isBetweenHalfOpen(T value, @Nullable T start, @Nullable T end) {
         return (start == null || value.compareTo(start) >= 0) && (end == null || value.compareTo(end) < 0);
+    }
+
+    public static Calendar convertToCalendar(LocalDateTime localDateTime) {
+        if (localDateTime == null)
+            return null;
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.clear();
+        calendar.set(localDateTime.getYear(), localDateTime.getMonthValue() - 1, localDateTime.getDayOfMonth(),
+                localDateTime.getHour(), localDateTime.getMinute(), localDateTime.getSecond());
+        return calendar;
+    }
+
+    public static Date convertToDate(LocalDateTime localDateTime) {
+        if (localDateTime == null)
+            return null;
+
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
     }
 }
